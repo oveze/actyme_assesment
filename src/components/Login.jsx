2c6a8b62bf53efc0f2913f6d85167e7ea4a5eaf1
Mohammed Oveze <mdoveze786@gmail.com>
2025-08-09 10:43:17 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = ({ setUser }) => {
     setError('');
 
     try {
-      const res = await axios.post('https://actyme-assesment-wuke.vercel.app//api/auth/login', {
+      const res = await axios.post('https://actyme-assesment-backe.onrender.com/api/auth/login', {
         email,
         password
       });
@@ -173,4 +173,5 @@ const Login = ({ setUser }) => {
 
 
 export default Login;
+
 
